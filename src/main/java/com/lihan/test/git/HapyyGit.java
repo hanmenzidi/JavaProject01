77631354477525714a2bc39376aa59a25c4e9c2b
tom_pro <goodMorning_pro@arguigu.com>
2020-10-30 23:36:20 +0800

    尝试解决chongtu1

--- src/main/java/com/lihan/test/git/HapyyGit.java
+++ src/main/java/com/lihan/test/git/HapyyGit.java
@@ -4,7 +4,7 @@ public class HapyyGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("djfdfjl");
+System.out.println("djfdfjl   baibing");
 System.out.println("jhdlfj");
 	}
 
